e836afd8d6a6a490e775a386a7d0993cf3e9d19a
Guilherme Santana <g.santossantana22@gmail.com>
2022-04-27 08:37:13 -0300

    exercicio js

--- 08-Eventos/eventos.js
+++ 08-Eventos/eventos.js
@@ -4,3 +4,11 @@ let resultado = document.getElementById("resultado");
 function clique(){
     resultado.textContent = nome.value;
 }
+
+let moeda = document.getElementById("moeda");
+let resultadoMoeda = document.getElementById("resultadoMoeda");
+
+function converter(){
+    return  resultadoMoeda.textContent = moeda.value * 5;
+    
+}
