bcf4f4259525c611cb05e6dfb12234ec1d5a2afe
Guilherme Santana <g.santossantana22@gmail.com>
2022-04-28 08:39:03 -0300

    conclusão do exercício

--- 08-Eventos/eventos.js
+++ 08-Eventos/eventos.js
@@ -5,16 +5,28 @@ function clique(){
     resultado.textContent = nome.value;
 }
 
+
+
+
 let moeda = document.getElementById("moeda");
 let resultadoMoeda = document.getElementById("resultadoMoeda");
 
 function converter(){
-    return  resultadoMoeda.textContent = moeda.value * 5;
-    
-}
-let formataValor = valor => {
-    return valor.toLocaleString('pt-br',{
+    return  resultadoMoeda.textContent = (moeda.value * 4.95).toLocaleString('pt-br',{
         style: 'currency',
         currency:'BRL'
-    });
+    }); ;
+    
 };
+
+// function converter(){
+//     resultadoMoeda.textContent = formataMoeda(moeda.value * 4.95);
+    
+    
+// };
+// function formataMoeda(valor){
+//     return valor.toLocaleString('pt-br',{
+//         style: 'currency',
+//         currency:'BRL'
+//     });
+// };
