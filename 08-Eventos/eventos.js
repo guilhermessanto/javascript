4d0cf5c670dba5dbdaca6216856e44c5aaa2e510
Guilherme Santana <g.santossantana22@gmail.com>
2022-04-28 09:26:41 -0300

    atualização js
    criação da pasta exercícios
    atualização da pasta eventos

--- 08-Eventos/eventos.js
+++ 08-Eventos/eventos.js
@@ -1,32 +1,5 @@
-let nome = document.getElementById("nome");
-let resultado = document.getElementById("resultado");
-
-function clique(){
-    resultado.textContent = nome.value;
-}
+let qualquerCoisa = document.getElementById("subtitulo-exemplo01");
+qualquerCoisa.style.color = "red";
+qualquerCoisa.innerHTML = "Qualquer coisa";
 
 
-
-
-let moeda = document.getElementById("moeda");
-let resultadoMoeda = document.getElementById("resultadoMoeda");
-
-function converter(){
-    return  resultadoMoeda.textContent = (moeda.value * 4.95).toLocaleString('pt-br',{
-        style: 'currency',
-        currency:'BRL'
-    }); ;
-    
-};
-
-// function converter(){
-//     resultadoMoeda.textContent = formataMoeda(moeda.value * 4.95);
-    
-    
-// };
-// function formataMoeda(valor){
-//     return valor.toLocaleString('pt-br',{
-//         style: 'currency',
-//         currency:'BRL'
-//     });
-// };
---
+++ exercicio/exercicio.js
@@ -0,0 +1,17 @@
+let moeda = document.getElementById("moeda");
+let resultadoMoeda = document.getElementById("resultadoMoeda");
+function converter(){
+    return  resultadoMoeda.textContent = (moeda.value * 4.95).toLocaleString('pt-br',{
+        style: 'currency',
+        currency:'BRL'
+    }); ;
+};
+function converter(){
+    resultadoMoeda.textContent = formataMoeda(moeda.value * 4.95); 
+};
+function formataMoeda(valor){
+    return valor.toLocaleString('pt-br',{
+        style: 'currency',
+        currency:'BRL'
+    });
+};
