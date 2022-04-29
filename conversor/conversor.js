ace6cf1afc8eded88f6aee568da9795928209447
Guilherme Santana <g.santossantana22@gmail.com>
2022-04-29 09:35:34 -0300

    Conversor de Moeda
    finalização solução 02

--- conversor/conversor.js
+++ conversor/conversor.js
@@ -1,7 +1,18 @@
-function converteDolar(){
+//Solução 01
+/*function converteDolar(){
     //pegando o valor digitado no campo valorDolar do HTML
     let valorDolar = document.getElementById('valorDolar').value;
     let converteReal = valorDolar * 4.97;
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `O valor convetido em Reais é R$ ${converteReal}`;
+    resultado.style.color = "red";
+};*/
+//solução 02
+function converteDolar(){
+    //pegando o valor digitado no campo valorDolar do HTML
+    let valorDolar = document.getElementById('valorDolar').value;
+    let cotacaoDolar = document.getElementById('cotacaoDolar').value;
+    let converteReal = valorDolar * cotacaoDolar;
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = `O valor convetido em Reais é R$ ${converteReal}`;
     resultado.style.color = "red";
