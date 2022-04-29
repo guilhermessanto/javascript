b95c733e14752aab9857b98b38faf7ecd0ba38d4
Guilherme Santana <g.santossantana22@gmail.com>
2022-04-29 10:24:52 -0300

    conversor
    Finalização do exemplo 03

--- conversor/conversor.js
+++ conversor/conversor.js
@@ -8,6 +8,7 @@
     resultado.style.color = "red";
 };*/
 //solução 02
+/*
 function converteDolar(){
     //pegando o valor digitado no campo valorDolar do HTML
     let valorDolar = document.getElementById('valorDolar').value;
@@ -17,3 +18,19 @@ function converteDolar(){
     resultado.innerHTML = `O valor convetido em Reais é R$ ${converteReal}`;
     resultado.style.color = "red";
 };
+*/
+//solução 03
+let formulario = document.querySelector('form');
+
+formulario.addEventListener('submit', function(event){
+    //previne o comportamento padrão do formulário ao enviar
+    event.preventDefault();
+    let cotacao = parseFloat(document.getElementById('cotacao').value);
+    let valor = parseFloat(document.getElementById('valor').value);   
+    let converte =  (valor * cotacao).toFixed(2);
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `O valor convertido é ${converte}`;
+
+}
+);
