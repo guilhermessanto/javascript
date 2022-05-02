2eefac681399dd9d5b87aa1eadeb500b6b39cdb6
Guilherme Santana <g.santossantana22@gmail.com>
2022-05-01 22:05:18 -0300

    conclusão advinha

--- ex02-advinha/advinha.js
+++ ex02-advinha/advinha.js
@@ -1,23 +1,34 @@
 let formulario = document.querySelector('form');
 let numeroEscolhido = parseInt(Math.random()*11);
+let btn = document.querySelector('button');
+let i = 0;
 
-formulario.addEventListener('submit', function(event){
-    event.preventDefault();
 
+btn.onclick = function(){
     
+    formulario.addEventListener('submit', function(event){
+    event.preventDefault();
     let palpiteUsuario = parseInt(document.getElementById('palpite').value);
     let resultado = document.getElementById('resultado');
-
-    if(palpiteUsuario == numeroEscolhido){
-        resultado.innerHTML = "Você acertou!";
-    }else if(palpiteUsuario <0 || palpiteUsuario >10 ){
-        resultado.innerHTML = "Digite um número entre 0 e 10";
-        
-    }else if(palpiteUsuario < numeroEscolhido){
-        resultado.innerHTML = "Errou Seu palpite é menor que o numero sorteado";
-    }else if (palpiteUsuario > numeroEscolhid){
-        resultado.innerHTML = "Errou Seu palpite é maior que o numero sorteado";
+    
+    if( i < 4){  
+        if(palpiteUsuario == numeroEscolhido){
+            resultado.innerHTML = "Você acertou!";
+        }else if(palpiteUsuario < 0 || palpiteUsuario > 10 ){
+            resultado.innerHTML = "Digite um número entre 0 e 10";
+            
+        }else if(palpiteUsuario > numeroEscolhido){
+            resultado.innerHTML = "Errou Seu palpite é menor que o número sorteado";
+        }else{
+            resultado.innerHTML = "Errou Seu palpite é maior que o número sorteado";
+        };            
     }else{
-        resultado.innerHTML = "Errou ";
-    };
+        resultado.innerHTML = "LOSER, tente novamente recarregando a página";
+    }
 });
+    i++
+    console.log(i) 
+}
+
+
+
